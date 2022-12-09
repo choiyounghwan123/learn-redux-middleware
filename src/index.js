b6e217498fec9c93b769ec77ea3f3ec165e95201
choiyounghwan <egoing@gmail.com>
2022-12-09 19:45:53 +0900

    redux-thunk start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import rootReducer from "./modules";
+import { Provider } from "react-redux";
+import loggerMiddleware from "./lib/loggerMiddleware";
+import { createLogger } from "redux-logger";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
